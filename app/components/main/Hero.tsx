cec87a4296e3e34e38c64d0e30e9c0edc62b05b1
abdelilleh <143023531+Moncif2005@users.noreply.github.com>
2024-02-24 21:17:25 +0100

    Add files via upload

---
+++ app/components/main/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeroContent from './HeroContent'
+
+function Hero() {
+  return (
+    <div className='realative flex flex-col h-full w-full'>
+        <HeroContent/>
+    </div>
+  )
+}
+
+export default Hero
